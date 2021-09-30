36753d524c13cc1d81a4edbb5ee098dfc8291ec2
alexmarufu <60462677+alexmarufu@users.noreply.github.com>
2021-09-30 09:16:41 +0200

    Update validateuser.js

--- validateuser.js
+++ validateuser.js
@@ -2,13 +2,14 @@
 const requireLogin = (req, res, next) => {
 
     const userId = req.body.userId;
+    const apikey = req.body.apikey;
 
-    if (!userId) {
-       return res.status(400).json({ message: "Authorization required" });
-    } else if(userId) {
-         req.userId = userId;
+    if (!userId && !apikey) {
+       return res.status(400).json({ message: "userId and apikey required" });
+    } else if(userId && apikey) {
+         req.user = { userId, apikey };
     } else {
-        return res.status(400).json({ message: "invalid user" });
+        return res.status(400).json({ message: "invalid userId or apikey " });
     }
     next();
     
